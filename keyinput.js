e5207e588c18ce457622ac4735e7fe393abb3ab9
coffeenotfound <coffeenotfound@users.noreply.github.com>
2017-01-11 00:29:25 +0100

    Update everything

--- keyinput.js
+++ keyinput.js
@@ -1,35 +1,34 @@
 var KeyInput = function(config) {
 	this.target = (typeof config == "array" && config.target !== undefined ? config.target : document);
 	
-	const MAX_POLL_ID = 1000000000;
-	
-	this.pollID = 0;
 	this.keystatesPress = new Array(256);
 	this.keystatesRelease = new Array(256);
+	
+	this.lastpoll = 0;
+	this.thispoll = 0;
+	
+	// prime arrays
+	var now = performance.now();
+	for(var i = 0; i < this.keystatesPress.length; i++) {
+		this.keystatesPress[i] = now;
+		this.keystatesRelease[i] = now;
+	}
 	
 	var self = this;
 	
 	// public methods
-	this.isKeyDown = function(key) {
-		return this.keystatesPress[key] >= (this.pollID - 1);
+	this.isKeyDown = function(key, duration) {
+		var duration = duration || 0;
+		
+		return this.keystatesPress[key] > this.keystatesRelease[key] + duration;
 	};
-	this.isKeyUp = function(key) {
-		return this.keystatesRelease[key] >= (this.pollID - 1);
+	this.isKeyUp = function(key, duration) {
+		return !this.isKeyDown(key, duration); // ensure it always returns the opposite of isKeyDown
 	};
 	this.poll = function() {
-		// increment poll id
-		this.pollID++;
-		
-		// manually reset array when poll id overflows
-		if(this.pollID >= MAX_POLL_ID) {
-			this.pollID = 0;
-			for(var i = 0; i < this.keystatesPress.length; i++) {
-				this.keystatesPress[i] -= MAX_POLL_ID;
-			}
-			for(var i = 0; i < this.keystatesRelease.length; i++) {
-				this.keystatesRelease[i] -= MAX_POLL_ID;
-			}
-		}
+		// set new poll timestamp
+		this.lastpoll = this.thispoll;
+		this.thispoll = performance.now();
 	};
 	this.hasFocus = function() {
 		return document.hasFocus();
@@ -48,24 +47,27 @@ var KeyInput = function(config) {
 		if(e.repeat == true) return;
 		
 		// set
-		self.keystatesPress[key] = self.pollID;
+		self.keystatesPress[key] = performance.now();
 	}, true);
 	
 	// on keyup
 	this.target.addEventListener('keyup', function(e) {		
 		var key = e.keyCode;
 		
-		e.preventDefault();
+		// allow f keys
+		if(key < 112 || key > 123) e.preventDefault();
 		
 		// set
-		self.keystatesRelease[key] = self.pollID;
+		self.keystatesRelease[key] = performance.now();
 	}, true);
 	
-	// on blur
+	// on focus lose
 	window.onblur = function(e) {
-		//if(self.releaseKeysOnBlur) {
-		self.pollID++;
-		//}
+		// release all keys
+		var now = performance.now();
+		for(var i = 0; i < self.keystatesRelease.length; i++) {
+			self.keystatesPress[i] = now;
+		}
 	};
 	
 	// try request focus
